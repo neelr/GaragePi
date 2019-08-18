6ba2db55cf172a33092ca0b233adcd0a977d52f6
Neel Redkar <neel.redkar@outlook.com>
2019-08-18 13:16:52 -0700

    add express and webserver

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var Gpio = require('onoff').Gpio;
+var express = require("express");
+var bodyparse = require("body-parser")
+var Gpio = require('onoff').Gpio
+var app = express();
 var door = new Gpio(4, 'out');
-
+// setup the stuff for gpio
 function open() {
 	door.writeSync(1);
 }
@@ -13,5 +16,19 @@ function activate() {
 	console.log(door.readSync());
 	setTimeout(close,5000)
 }
+app.use(bodyparse.json());
+app.get("/",(req,res)=> {
+	res.send("Hi! This is garage pi!");
+});
+app.post("/door",(req,res)=> {
+	if (req.body.auth == process.env.KEY) {
+		res.sendStatus(200)
+		activate();
+	} else {
+		res.sendStatus(401)
+	}
+});
 
-activate();
+app.listen(3000,()=> {
+	console.log("On port 3000");
+})
